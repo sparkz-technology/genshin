e397437cbba7a23e081afd24f887ee39906296cd
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-28 16:48:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { getScreenData } from "@/lib/action";
 import GenshinRedeemContainer from "./container";
 
+export const dynamic = "force-dynamic";
+
 export default async function GenshinRedeemPage() {
   const data = await getScreenData();
   if (!data || !data.settings) {
