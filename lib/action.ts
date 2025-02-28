5171a826ba0bddc521c5f0ca884a2aad49a20c05
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-02-28 14:16:09 +0530

    Update action.ts

--- lib/action.ts
+++ lib/action.ts
@@ -63,7 +63,14 @@ async function redeemCode(cdkey: string): Promise<boolean> {
     const response = await fetch(`${baseUrl}?${queryParams.toString()}`, {
       headers: {
         Cookie: cookies,
-        "User-Agent": "Mozilla/5.0",
+         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.9",
+          "Referer": "https://webstatic-sea.hoyoverse.com/",
+          "Origin": "https://webstatic-sea.hoyoverse.com",
+          "Connection": "keep-alive",
+          "DNT": "1",
+          "Upgrade-Insecure-Requests": "1"
       },
     });
 
