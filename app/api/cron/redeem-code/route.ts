25832f31e5431d0ffbf5a0e9d3b9fa22ad123717
sutharsan <62829664+sparkz-technology@users.noreply.github.com>
2025-03-14 09:41:33 +0000

    Await processCodes execution in GET handler for proper asynchronous processing

--- app/api/cron/redeem-code/route.ts
+++ app/api/cron/redeem-code/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    processCodes()
+    await processCodes()
     return NextResponse.json({ status: 200, message: "Processing codes..." });
   } catch {
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
